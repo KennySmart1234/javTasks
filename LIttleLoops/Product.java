7b5e210d616f54c42ade2d0923a63e63ca228970
Kenny Smart <kennysmart1234@gmail.com>
2026-04-13 08:08:55 +0100

    Nokia3310

--- LIttleLoops/Product.java
+++ LIttleLoops/Product.java
@@ -1,10 +1,11 @@
 public class Product{
 public static void main(String... agrs){
 
-
+      int product = 1;
       int i = 1;
         while (i <= 10){
-            System.out.println(i*i);
+            System.out.println(product);
+            product = product * i;
             i++;
      }
 
