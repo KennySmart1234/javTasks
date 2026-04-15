3e5472e989292aeb2f29b7fccc7aa60899c03a21
Kenny Smart <kennysmart1234@gmail.com>
2026-04-15 17:07:40 +0100

    Fire Drill

---
+++ FillDrill2/TaskOne.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class TaskOne{
+public static void main(String[] agrs){
+
+         Scanner inputCollector = new Scanner(System.in);
+
+    System.out.print("Enter scores ");
+int score = inputCollector.nextInt();
+
+
+int scores = 0;
+while(scores <= score){
+    System.out.print("Enter scores ");
+    scores++;
+     System.out.print(scores);
+
+
+}
+
+
+}
+
+
+
+}
+
+
+
+
+
+
+
+
